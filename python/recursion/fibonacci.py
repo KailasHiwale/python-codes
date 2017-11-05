f47313cfa55e93af1b73e3142569e66986615317
KailasHiwale <kailashiwale91@gmail.com>
2017-11-05 20:58:15 +0530

    update: fobonacci.py

--- python/recursion/fibonacci.py
+++ python/recursion/fibonacci.py
@@ -1,11 +1,12 @@
-# Hint: Fibonacci Sequese n = (n-1) + (n-2), Where n is next number
+# Hint: Fibonacci Sequese Fn = F(n-1) + F(n-2), Where Fn is next number
+from __future__ import print_function
 
 
-def fibonacci_sequence(num):
-    if num <= 1:
-        return num
+def fibonacci_sequence(n):
+    if n <= 1:
+        return n
     else:
-        return fibonacci_sequence(num - 1) + fibonacci_sequence(num - 2)
+        return fibonacci_sequence(n - 1) + fibonacci_sequence(n - 2)
 
 
 num = int(input("Enter positive number:"))
@@ -16,4 +17,5 @@ else:
     print("Fibonacci Sequence: ")
     for i in range(num):
         print(fibonacci_sequence(i), end=" ")
+
 
