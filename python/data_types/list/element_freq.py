2c10ed37cf98d915270ac8f8ba837ba1cf32c1b1
kh99 <kailash.hiwale@powerupcloud.com>
2018-10-23 12:41:49 +0530

    data types updated

--- python/data_types/list/element_freq.py
+++ python/data_types/list/element_freq.py
@@ -1,9 +1,13 @@
-li = [2, 4, 2, 1, 5, 3, 2, 2, 6]
-di = {}
+def element_frequency(li):
+    """Function to find frequency of each element of list"""
+    di = {}
+    for i in li:
+        di[i] = li.count(i)
+    return di
 
-for i in li:
-    di[i] = li.count(i)
 
+li = input("Enter list i.e 1,2,3,1: ").split(",")
+di = element_frequency(li)
 print("Element Frequency: ")
 for key, value in di.items():
     print("{}: {}".format(key, value))
--- python/data_types/list/max_min_list.py
+++ python/data_types/list/max_min_list.py
@@ -1,10 +1,13 @@
-# Max element in the list without using any inbuilt function
+# Max/Min element in the list without using any inbuilt function
 li = [2, 4, 1, 2, 11, 10, 1, 2, 5]
 
 
 def max_min(li):
+    """
+    Function to find max/min element in the list without
+    using any inbuilt function.
+    """
     max, min, i = li[0], li[0], 1
-
     for i in li:
         if i >= max:
             max = i
